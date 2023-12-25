5a05026fcdab15c6152135b22df9196059ec5067
tuyennv <nguyenvantuyenx2bk@gmail.com>
2023-12-25 11:02:40 +0700

    change data list music

--- README.md
+++ README.md
@@ -1,71 +1,54 @@
-deploy một project React-Vite lên GitHub bằng GitHub Pages
-
-1. Tạo một repository trên GitHub cho project .
-2. Clone repository đó về máy tính của bạn bằng cách sử dụng lệnh git clone trong terminal.
-
-git clone https://github.com/your-username/your-repository.git
+# Deploy project React-Vite in GitHub Pages
 
 
-3. Di chuyển vào thư mục của repository bằng lệnh cd your-repository.
-
-cd your-repository
-
-
-4. Trong thư mục gốc của project React-Vite, tạo một file .env và đặt nội dung sau:
-
+In source project React-Vite, create file .env :
+``` 
 VITE_BASE_URL=/your-repository-name/
+```
 
 
-5. Trong file vite.config.js, thêm cấu hình base để xác định base URL cho GitHub Pages:
-
+In file vite.config.js, add config base to define URL for GitHub Pages:
+```
 export default {
   base: '/your-repository-name/',
   // ... other config settings
 }
+```
 
 
-6. Tiếp theo, cài đặt package vite-plugin-gh-pages để giúp deploy project lên GitHub Pages.
-
+next, install package vite-plugin-gh-pages for deploy project in GitHub Pages.
+```
 npm install vite-plugin-gh-pages --save-dev
+```
 
 
-7. Thêm plugin vite-plugin-gh-pages vào file vite.config.js:
-
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
-import { ViteGlobInputPlugin } from 'vite-plugin-glob-input'
-import VitePluginPages from 'vite-plugin-pages'
-import VitePluginVuedoc from 'vite-plugin-vuedoc'
-import VitePluginComponents from 'vite-plugin-components'
-import VitePluginWindicss from 'vite-plugin-windicss'
+add plugin vite-plugin-gh-pages to file vite.config.js:
+```
 import VitePluginGhPages from 'vite-plugin-gh-pages' // Add this line
 
 export default defineConfig({
   plugins: [
-    VitePWA(),
-    ViteGlobInputPlugin(),
-    VitePluginPages(),
-    VitePluginVuedoc(),
-    VitePluginComponents(),
-    VitePluginWindicss(),
     VitePluginGhPages() // Add this line
   ]
 })
+```
 
 
-8. Tạo một script trong file package.json để build và deploy project lên GitHub Pages.
-
+create script in file package.json to build and deploy project.
+```
 "scripts": {
   "build": "vite build",
   "deploy": "npm run build && npx vite build --base /your-repository-name/ && npx gh-pages -d dist"
 }
+```
 
-
-Thay your-repository-name bằng tên của repository của bạn trên GitHub.
-
-9. Chạy script deploy bằng lệnh:
-
+Run script deploy cmd:
+```
 npm run deploy
+```
 
 
-Sau khi hoàn thành các bước trên, project React-Vite của bạn sẽ được deploy lên GitHub Pages và có thể truy cập qua đường dẫn https://your-username.github.io/your-repository.
+Error "gh-pages' is not recognized as an internal or external command, operable program or batch file"  the reason may be package gh-pages not install 
+```
+npm install gh-pages --save-dev
+```
--- src/data.js
+++ src/data.js
@@ -2,21 +2,21 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
-      name: "Beaver Creek",
+      name: "Cắt Đôi Nỗi Sầu",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10075",
+        "https://loremflickr.com/400/400/cats",
+      artist: "by me",
+      audio: "https://vnso-zn-16-tf-a128-zmp3.zmdcdn.me/49e28a31e7fee3089127f25498cad799?authen=exp=1703649383~acl=/49e28a31e7fee3089127f25498cad799/*~hmac=c3651d400e4cc741d72e6182a1e3a3db",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: true,
     },
     {
-      name: "Daylight",
+      name: "Ngày Mai Người Ta Lấy Chồng",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
-      artist: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=9272",
+        "https://loremflickr.com/400/400/cats",
+      artist: "by me",
+      audio: "https://a128-zmp3.zmdcdn.me/cc97b6199c6945d9efdb860f593d8842?authen=exp=1703649387~acl=/cc97b6199c6945d9efdb860f593d8842/*~hmac=82fd0712f7fe29e7c4c0ff618edcbc4f",
       color: ["#EF8EA9", "#ab417f"],
       id: uuidv4(),
       active: false,
@@ -24,7 +24,7 @@ function chillHop() {
     {
       name: "Keep Going",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+        "https://loremflickr.com/400/400/cats",
       artist: "Swørn",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9222",
       color: ["#CD607D", "#c94043"],
@@ -34,7 +34,7 @@ function chillHop() {
     {
       name: "Nightfall",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
+        "https://loremflickr.com/400/400/cats",
       artist: "Aiguille",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9148",
       color: ["#EF8EA9", "#ab417f"],
@@ -44,7 +44,7 @@ function chillHop() {
     {
       name: "Reflection",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
+        "https://loremflickr.com/400/400/cats",
       artist: "Swørn",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=9228",
       color: ["#CD607D", "#c94043"],
@@ -54,7 +54,7 @@ function chillHop() {
     {
       name: "Under the City Stars",
       cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+        "https://loremflickr.com/400/400/cats",
       artist: "Aso, Middle School, Aviino",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
       color: ["#205950", "#2ab3bf"],
