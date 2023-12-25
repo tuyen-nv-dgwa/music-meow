22d753ea89e2f6b41804da169f0f83e76446781d
tuyennv <nguyenvantuyenx2bk@gmail.com>
2023-12-25 09:39:46 +0700

    add deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginGhPages from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/music-meow/',
+  plugins: [react(),  VitePluginGhPages()],
 })
